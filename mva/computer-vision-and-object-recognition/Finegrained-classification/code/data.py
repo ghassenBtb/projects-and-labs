9b4df3337b3937d4d6aea1e608f46ac74fe660c2
Ghassen Btb <bettaieb.gha@gmail.com>
2020-12-28 22:20:02 +0100

    small updates

--- mva/computer-vision-and-object-recognition/Finegrained-classification/code/data.py
+++ mva/computer-vision-and-object-recognition/Finegrained-classification/code/data.py
@@ -3,10 +3,6 @@ import os
 
 import torchvision.transforms as transforms
 
-# once the images are loaded, how do we pre-process them before being passed into the network
-# by default, we resize the images to 64 x 64 in size
-# and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from
-# the training set
 
 class White_noise():
 	"""
